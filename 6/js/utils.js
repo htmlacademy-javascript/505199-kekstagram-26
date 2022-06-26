6d7b2935d8b3d5f594415fee4dbff390f2ac0c93
Keks <keks@htmlacademy.ru>
2022-06-26 14:55:08 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/utils.js
@@ -0,0 +1,25 @@
+//Получение случайного числа в выбранном диапазоне
+
+const getRandomInt = function (min, max) {
+  if (min >= 0 && min < max) {
+    const rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  }
+  throw new Error('Переданный диапазон чисел введен некорректно');
+};
+
+getRandomInt(15, 48);
+
+// Ограничение длины строки
+
+const checkStringLength = function (currentString, maxLength) {
+  return currentString.length <= maxLength;
+};
+
+checkStringLength('she sells seashels', 20);
+
+// Функция прерывания или закрытия текущего процесса
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export { getRandomInt, checkStringLength, isEscapeKey };
